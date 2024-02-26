d682d0263ea9f896614c9a093cdbaa22b401333a
Beck Yuldashev <yuldashev.bek@gmail.com>
2024-02-26 17:46:04 +0300

    [feature/RF-7] create a file with css variables

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { appRouter } from './appRouter'
 
+import '@/shared/styles/theme.css'
+import '@/shared/styles/global.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={appRouter} />
