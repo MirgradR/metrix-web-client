ee79344fc5ae9eab375f0eb5d1c3b723487cc2e4
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2024-02-26 20:27:42 -0800

    [feature/RF-10] made an icon component and added 5 icons

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -2,6 +2,7 @@ declare module '*.module.css' {
   interface IClassNames {
     [className: string]: string
   }
+
   const classNames: IClassNames
   export = classNames
 }
@@ -11,7 +12,6 @@ declare module '*.jpg'
 declare module '*.jpeg'
 declare module '*.svg' {
   import React from 'react'
-
-  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>
+  const SVG: React.FC<React.SVGProps<SVGSVGElement>>
   export default SVG
 }
