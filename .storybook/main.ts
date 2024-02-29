e2ba7b95d88f8b78621aedb8518d1b4101a25f98
Beck Yuldashev <yuldashev.bek@gmail.com>
2024-02-29 21:46:55 +0300

    [feature/RF-6] Setting up UI Kit inside a project

---
+++ .storybook/main.ts
@@ -0,0 +1,42 @@
+import type { StorybookConfig } from '@storybook/react-webpack5'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions'
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {
+      builder: {
+        useSWC: true
+      }
+    }
+  },
+  docs: {
+    autodocs: 'tag'
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack'
+        },
+        {
+          loader: 'file-loader'
+        }
+      ],
+      type: 'javascript/auto',
+      issuer: {
+        and: [/\.(ts|tsx|js|jsx|md|mdx)$/]
+      }
+    })
+
+    return config
+  }
+}
+export default config
