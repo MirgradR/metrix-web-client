4d87d063fe993d58a879e12df81db4907e66e3eb
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2024-03-02 11:37:28 -0800

    [feature/RF-10] create Icon component

---
+++ src/shared/ui/Icon/Icon.model.ts
@@ -0,0 +1,8 @@
+import icons from '@/shared/ui/Icon/assets'
+
+export interface IconProps {
+  icon: keyof typeof icons
+  color?: string
+  size?: '16' | '20' | '24' | '36' | '60'
+  className?: string
+}
