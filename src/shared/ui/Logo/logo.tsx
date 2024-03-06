52a9c6b6a9518ea45ec7b0c1a37d7b2d58f21c0c
a.svirin <a.svirin94@gmail.com>
2024-03-06 20:25:51 +0300

    naming fix

--- src/shared/ui/Logo/logo.tsx
+++ src/shared/ui/Logo/logo.tsx
@@ -7,7 +7,7 @@ const Logo = ({ size = '52', withText = true }: LogoProps) => {
   return (
     <div className="logo">
       <Icon icon={'logo'} size={size} viewBox={'0 0 52 52'} />
-      {withText && <span className="name">Metrix</span>}
+      {withText && <span className="logo__title">Metrix</span>}
     </div>
   )
 }
