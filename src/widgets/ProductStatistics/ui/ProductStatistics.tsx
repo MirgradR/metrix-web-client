f10287aa91197feaeced6a30c6de1abe6fc29720
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2024-03-18 09:48:51 -0700

    [feature/RF-48] create a product statistics widget

---
+++ src/widgets/ProductStatistics/ui/ProductStatistics.tsx
@@ -0,0 +1,36 @@
+import classNames from '@/shared/lib/utils/classNames'
+import SummaryCardValues from '@/shared/ui/SummaryCardValues'
+import React from 'react'
+import Block from '@/shared/ui/Block/Block'
+import Icon from '@/shared/ui/Icon/Icon'
+import Text from '@/shared/ui/Text/Text'
+import styles from '@/shared/ui/SummaryCard/styles.module.css'
+import { ProductStatisticsProps } from './productStatistics.model'
+
+const ProductStatistics = ({ period, allProducts, diffAllProducts, active, diffActive }: ProductStatisticsProps) => {
+  return (
+    <Block className={styles.blockHover}>
+      <div className={styles.summaryCardHeader}>
+        <div className={classNames(styles.iconBlock, styles['iconBlock--blueBackground'])}>
+          <Icon icon="folder" size="20" />
+        </div>
+        <Text type="label1" weight="regular">
+          {period && (
+            <div className={styles.period}>
+              <Text type="label1" weight="regular" className={styles.text}>
+                {period}
+              </Text>
+              <Icon icon="chevronDown" size="16" />
+            </div>
+          )}
+        </Text>
+      </div>
+      <div className={styles.summaryColumns}>
+        <SummaryCardValues title="All Products" value={allProducts} difference={diffAllProducts} />
+        <SummaryCardValues title="Active" value={active} difference={diffActive} />
+      </div>
+    </Block>
+  )
+}
+
+export default ProductStatistics
